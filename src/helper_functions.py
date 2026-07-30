177a49ea1dec3af0a015ef53c034c12e323e4b3a
milaS-07 <mila.starovic07@gmail.com>
2026-07-30 02:28:10 +0200

    popravljen helper_functions fajl

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,5 +1,4 @@
 def calculate_ghz_fidelity(counts: dict, shots: int, num_qubits: int) -> float:
-    """Rastojanje/fidelitet za GHZ stanje (|00...0> + |11...1>)"""
     ideal_state_0 = "0" * num_qubits
     ideal_state_1 = "1" * num_qubits
 
@@ -8,7 +7,6 @@ def calculate_ghz_fidelity(counts: dict, shots: int, num_qubits: int) -> float:
 
 
 def calculate_tvd(counts_ideal: dict, counts_noisy: dict, shots: int) -> float:
-    """Total Variation Distance (TVD) između idealne i šumovite raspodele"""
     all_keys = set(counts_ideal.keys()).union(set(counts_noisy.keys()))
     tvd = 0.0
     for k in all_keys:
